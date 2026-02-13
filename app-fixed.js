85f36e484d4747898f3494c666af3535c0e68104
Auto1412 <145248890+Auto1412@users.noreply.github.com>
2026-02-13 09:59:48 +0700

    Refactor Supabase client initialization and voting logic

--- app-fixed.js
+++ app-fixed.js
@@ -3,7 +3,7 @@ const { createClient } = supabase;
 
 const supabaseClient = createClient(
     "https://onyapxclnfsdgcwisnhx.supabase.co",
-    "sb_publishable_j7FRmHfGnLhI3ZbTzK7WIQ_wid_0q_Q"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ueWFweGNsbmZzZGdjd2lzbmh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4ODQzMDksImV4cCI6MjA4NjQ2MDMwOX0.MkzFOJ_Ucs_t7Led5smGsj4deX_rtPbAHXAD_BaI-ns"
 );
 
 // Global variables
@@ -520,6 +520,7 @@ if (document.readyState === 'loading') {
 } else {
     init();
 }
+
 
 
 
