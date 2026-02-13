571f6fb24a5bb67b0ceafa002fd3ad45a792cc60
Auto1412 <145248890+Auto1412@users.noreply.github.com>
2026-02-13 10:39:29 +0700

    Change Supabase API key in app-fixed.js
    Updated Supabase client initialization with a new API key.

--- app-fixed.js
+++ app-fixed.js
@@ -6,7 +6,7 @@ const { createClient } = supabase;
 
 const supabaseClient = createClient(
     "https://onyapxclnfsdgcwisnhx.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ueWFweGNsbmZzZGdjd2lzbmgiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc3MDg4NDMwOSwiZXhwIjoyMDg2NDYwMzA5fQ.MkzFOJ_Ucs_t7Led5smGsj4deX_rtP[...]"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ueWFweGNsbmZzZGdjd2lzbmh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4ODQzMDksImV4cCI6MjA4NjQ2MDMwOX0.MkzFOJ_Ucs_t7Led5smGsj4deX_rtPbAHXAD_BaI-ns"
 );
 
 // Global variables
